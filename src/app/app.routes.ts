96464742d75014318e8d067cb2d36d0486da0f45
Natthaphon Ditthaviboon <bug9best@gmail.com>
2025-09-07 18:16:30 +0700

    add task component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Task } from '../pages/task/task';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'task',
+        pathMatch: 'full'
+    },
+    {
+        path: 'task',
+        component: Task
+    }
+];
