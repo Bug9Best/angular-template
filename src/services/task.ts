d71316edb97e5205ace711f5e2561a4993e70798
Natthaphon Ditthaviboon <bug9best@gmail.com>
2025-09-08 02:36:49 +0700

    update service

--- src/services/task.ts
+++ src/services/task.ts
@@ -1,38 +1,36 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
-  listTasks: any[] = [
-    { id: 1, title: 'Task 1', completed: false },
-    { id: 2, title: 'Task 2', completed: true },
-    { id: 3, title: 'Task 3', completed: false },
-    { id: 4, title: 'Task 4', completed: true },
-    { id: 5, title: 'Task 5', completed: false }
-  ];
+  apiPath: string = environment.apiUrl + '/tasks';
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
 
   getTasks() {
-    return this.listTasks;
+    return this.httpClient.get<any[]>(`${this.apiPath}/getTasks`);
   }
 
   getTaskById(id: number) {
-    return this.listTasks.find(task => task.id === id);
+    return this.httpClient.get<any>(`${this.apiPath}/getTaskById/${id}`);
   }
 
-  createTask(task: any) {
-    this.listTasks.push(task);
+  createTask(param: any) {
+    return this.httpClient.post<any>(`${this.apiPath}/createTask`, param);
   }
 
-  updateTask(id: number, updatedTask: any) {
-    const index = this.listTasks.findIndex(task => task.id === id);
-    if (index !== -1) {
-      this.listTasks[index] = { ...this.listTasks[index], ...updatedTask };
-    }
+  updateTask(id: number, param: any) {
+    return this.httpClient.put<any>(`${this.apiPath}/updateTask/${id}`, param);
   }
 
   deleteTask(id: number) {
-    this.listTasks = this.listTasks.filter(task => task.id !== id);
+    return this.httpClient.delete<any>(`${this.apiPath}/deleteTask/${id}`);
   }
 }
